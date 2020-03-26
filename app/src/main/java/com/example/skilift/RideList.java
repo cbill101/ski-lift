7fac894471591f0e4ac5334e7525d9c7386f4b05
Anderson Porta <anderson.porta@outlook.com>
2020-03-26 14:53:40 -0600

    Retrieve list of rides implemented. However, UI looks weird when displaying list of rides

--- app/src/main/java/com/example/skilift/RideList.java
+++ app/src/main/java/com/example/skilift/RideList.java
@@ -1,19 +1,31 @@
 package com.example.skilift;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class RideList extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton radioButton;
     private Button confirmButton;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +34,37 @@ public class RideList extends AppCompatActivity {
 
         radioGroup = findViewById(R.id.radioGroup);
         confirmButton = findViewById(R.id.confirmButton);
+        listView = findViewById(R.id.listView);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { openPayment();
+            }
+        });
+
+        // Array list to save from db
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+
+        // Retrieve vals from db and display
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Providers");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Information info = snapshot.getValue(Information.class);
+                    String infoRow = "Name: " + info.getName() +
+                            "\nPhone: " + info.getPhone() +
+                            "\nPrice: " + info.getPrice();
+                    list.add(infoRow);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
